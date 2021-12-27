cf7c8fd3529a6fdd5351fa6ffcd1c7eaf518f193
Austin Ziegler <austin@zieglers.ca>
2021-12-26 23:40:29 -0500

    Update node packages

--- Node.fish
+++ Node.fish
@@ -1,9 +1,42 @@
-npm i -g @2fd/graphdoc @graphql-cli/codegen @graphql-cli/coverage @graphql-cli/diff @graphql-cli/generate \
-  @graphql-cli/introspect @graphql-cli/similar @graphql-cli/validate @graphql-inspector/cli apollo-codegen \
-  graphql graphql-cli graphql-markdown graphqldoc
-npm i -g @cyclonedx/bom
-npm i -g @redocly/openapi-cli insomnia-documenter raml2html raml2html-full-markdown-theme raml2html-markdown-theme \
-   raml2html-markdown-theme-schema raml2html-printable-theme raml2html-slate-theme redoc-cli swagger-cli
-npm i -g @vue/cli @vue/devtools
-npm i -g fx json5 logsene-cli prettier svgo
-npm i -g pnpm yarn
+npm i -g \
+    @2fd/graphdoc \
+    @cyclonedx/bom \
+    @graphql-cli/codegen \
+    @graphql-cli/coverage \
+    @graphql-cli/diff \
+    @graphql-cli/generate \
+    @graphql-cli/introspect \
+    @graphql-cli/similar \
+    @graphql-cli/validate \
+    @graphql-inspector/cli \
+    @redocly/openapi-cli \
+    @vue/cli \
+    @vue/devtools \
+    ajv-cli \
+    apollo-codegen \
+    fx \
+    graphql \
+    graphql-cli \
+    graphql-markdown \
+    graphqldoc \
+    insomnia-documenter \
+    install-peerdeps \
+    json5 \
+    knex \
+    logsene-cli \
+    pnpm \
+    prettier \
+    quicktype \
+    raml2html \
+    raml2html-full-markdown-theme \
+    raml2html-markdown-theme \
+    raml2html-markdown-theme-schema \
+    raml2html-printable-theme \
+    raml2html-slate-theme \
+    redoc-cli \
+    sass \
+    sass-migrator \
+    svgo \
+    swagger-cli \
+    typescript \
+    yarn
