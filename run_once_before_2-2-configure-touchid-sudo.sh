b1ab47a6b8b682856ca3dd309acd969d33a33277
Austin Ziegler <austin@zieglers.ca>
2022-10-03 10:55:31 -0400

    Fix a bug in run-once script

--- run_once_before_2-2-configure-touchid-sudo.sh
+++ run_once_before_2-2-configure-touchid-sudo.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 if [[ "$(uname -s)" == Darwin ]]; then
-  if ! grep -q pam_tid.so /etc/pam.d/sudo; then
-    osascript -e 'tell application "System Preferences" to quit'
-    sudo sed -i '' -e '2i\
-auth        sufficient     pam_tid.so' /etc/pam.d/sudo
-  fi
+  osascript -e 'tell application "System Preferences" to quit'
+
+  grep -q pam_tid.so /etc/pam.d/sudo || sudo sed -i '' -e '2i\
+auth       sufficient     pam_tid.so' /etc/pam.d/sudo
 fi
