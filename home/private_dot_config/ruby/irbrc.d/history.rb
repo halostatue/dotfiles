90b1230b3e7d3b6ff2215fc141ee5c5a2857b3b2
Austin Ziegler <austin@zieglers.ca>
2023-03-25 21:28:50 -0400

    Fix an issue with IRB/pry history
    - Also make IRB/pry history unique per path

--- home/private_dot_config/ruby/irbrc.d/history.rb
+++ home/private_dot_config/ruby/irbrc.d/history.rb
@@ -1,8 +1,13 @@
+require "digest/md5"
+
+pwd_base = Pathname.pwd.basename.to_s.sub(/^\./, "")
+pwd_digest = Digest::MD5.hexdigest(Dir.pwd)
+
 history = Pathname(ENV.fetch("XDG_CACHE_HOME") { Pathname(ENV["HOME"]).join(".cache") })
-  .join("ruby/irb_history").to_path
+  .join("ruby", "irb_history.#{pwd_base}.#{pwd_digest}").to_path
 
 if defined? Pry
   Pry.config.history_file = history
 else
-  IRB.conf[:SAVE_HISTORY] = history
+  IRB.conf[:HISTORY_FILE] = history
 end
